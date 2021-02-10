d977b79f39c6aa7cfe8c3440e5c10d456d2fe624
Manish Kumar <manish.hit2800@gmail.com>
2021-02-10 12:28:59 +0530

    css updated

--- src/App.js
+++ src/App.js
@@ -23,7 +23,23 @@ const App = () => {
   }
 
   const checkIsWinner = () => {
-
+    if(itemArray[0] === itemArray[1] && itemArray[0]===itemArray[2]&&itemArray[0]!=="empty") {
+      SetWinMessage(`${itemArray[0]} wins`)
+    } else if(itemArray[3] === itemArray[4] && itemArray[3]===itemArray[5]&&itemArray[3]!=="empty") {
+      SetWinMessage(`${itemArray[3]} wins`)
+    } else if(itemArray[6] === itemArray[7] && itemArray[6]===itemArray[8]&&itemArray[6]!=="empty") {
+      SetWinMessage(`${itemArray[6]} wins`)
+    } else if(itemArray[0] === itemArray[3] && itemArray[0]===itemArray[6]&&itemArray[0]!=="empty") {
+      SetWinMessage(`${itemArray[0]} wins`)
+    } else if(itemArray[1] === itemArray[4] && itemArray[1]===itemArray[7]&&itemArray[1]!=="empty") {
+      SetWinMessage(`${itemArray[1]} wins`)
+    } else if(itemArray[2] === itemArray[5] && itemArray[2]===itemArray[8]&&itemArray[2]!=="empty") {
+      SetWinMessage(`${itemArray[2]} wins`)
+    } else if(itemArray[0] === itemArray[4] && itemArray[0]===itemArray[8]&&itemArray[0]!=="empty") {
+      SetWinMessage(`${itemArray[0]} wins`)
+    } else if(itemArray[2] === itemArray[4] && itemArray[2]===itemArray[6]&&itemArray[2]!=="empty") {
+      SetWinMessage(`${itemArray[2]} wins`)
+    }
   }
 
   const changeItem = itemNumber => {
@@ -45,22 +61,22 @@ const App = () => {
     <Container className="p-5">
       <ToastContainer position="bottom-center" />
       <Row>
-        <Col md={6} className="offset-md-3">
+        <Col  md={6} className="offset-md-3">
           {winMessage ?(
             <div className="mb-2 mt-2">
               <h1 className="text-success text-uppercase text-center">
                 {winMessage}
               </h1>
-              <Button className="success" block onClick={reloadGame}>Reload the game</Button>
+              <Button className="success" block onClick={reloadGame}>Restart</Button>
             </div>
           ) :(
-            <h1 className="text-center">
+            <h1 color="warning" className="text-center">
               {isCross ? "Cross" : "Circle"} turns
             </h1>
           )}
           <div className="grid">
             {itemArray.map((item, index)=>(
-              <Card>
+              <Card color="info" onClick={()=> changeItem(index)}>
                 <CardBody className="box">
                   <Icon name={item}/>
                 </CardBody>
